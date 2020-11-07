e031ad3d7146436aef6b023e5391096f0bf09db2
Javohir Hakimov <57003102+javohirdev@users.noreply.github.com>
2020-11-07 18:47:40 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import StatLesson from './StatLesson/StatLesson';
+// import PostRequest from './components/PostRequest';
+// import Lesson1 from './components/Lesson1';
+// import Backend from './components/Backend';
+// import Recipe from './app/Recipe';
+// import Asosiy from './e-commence/Asosiy';
+import MainMovie from './movieapp/MainMovie';
+
+function App() {
+  return (
+    <div>
+      {/* <Lesson1 
+      title="Class Component"
+      data={{name: 'Javohir', age: 16}}
+      /> */}
+      {/* <Backend /> */}
+      {/* <Recipe /> */}
+      {/* <PostRequest /> */}
+      {/* <StatLesson /> */}
+      {/* <Asosiy /> */}
+      <MainMovie />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/movieapp/MainMovie.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Search from './Search';
+import Results from './Results';
+import axios from 'axios';
+import '../styles/MovieApp.css';
+
+function MainMovie() {
+
+    const [state, setState] = useState({
+        s: "",
+        results: [],
+        selected: {}
+    })
+    
+    const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=901daf37";
+
+    const search = (e) => {
+        if (e.key === "Enter") {
+          axios(apiUrl + "&s=" + state.s).then(({ data }) => {
+            let results = data.Search;
+    
+            setState(prevState => {
+              return { ...prevState, results: results }
+            })
+          });
+        }
+    }
+
+    const handleInput = (e) => {
+        let s = e.target.value;
+
+        setState(prevState => {
+            return {...prevState, s: s}
+        })
+    }
+
+    return (
+        <div className="MainApp">
+            <header>
+                <h3>Movie App with OMDb API</h3>
+            </header>
+            <Search handleInput={handleInput} search={search}/>
+            <Results results={state.results} />
+        </div>
+    )
+}
+
+export default MainMovie;
